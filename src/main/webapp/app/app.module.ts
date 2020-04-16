f7e40b5761585633d2f1af8ed33a059e8cf80e8b
Christoph Zuidema <czuidema@student.ethz.ch>
2020-04-17 00:48:11 +0200

    time picker working. formatting could be improved though.

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -14,10 +14,13 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   imports: [
     BrowserModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
     InstantHelpFinderSharedModule,
     InstantHelpFinderCoreModule,
     InstantHelpFinderHomeModule,
