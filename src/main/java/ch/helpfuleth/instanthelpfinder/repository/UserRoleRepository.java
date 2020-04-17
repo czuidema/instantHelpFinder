cd80839d6cbec52694132827c4090ee3f3aff91b
parrapbanzz <peyman.khodadust@gmail.com>
2020-04-17 10:08:04 +0200

    adds UserRoleService

--- src/main/java/ch/helpfuleth/instanthelpfinder/repository/UserRoleRepository.java
+++ src/main/java/ch/helpfuleth/instanthelpfinder/repository/UserRoleRepository.java
@@ -1,9 +1,13 @@
 package ch.helpfuleth.instanthelpfinder.repository;
 
+import ch.helpfuleth.instanthelpfinder.domain.TurningEvent;
 import ch.helpfuleth.instanthelpfinder.domain.UserRole;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Spring Data  repository for the UserRole entity.
@@ -11,4 +15,8 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+
+
+    @Query("select userRole from UserRole userRole left join fetch userRole.user user where user.login = :userLogin")
+    Optional<UserRole> findOneByUserLogin(@Param("userLogin") String userLogin);
 }
