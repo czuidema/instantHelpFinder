8b011bb222ad52b24d78b204e0b9339e821afe33
parrapbanzz <peyman.khodadust@gmail.com>
2020-05-10 11:46:36 +0200

    Activates source-map generation in prod profile

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -17,7 +17,8 @@ const sass = require('sass');
 module.exports = webpackMerge(commonConfig({ env: ENV }), {
     // Enable source maps. Please note that this will slow down the build.
     // You have to enable it in Terser config below and in tsconfig.json as well
-    // devtool: 'source-map',
+    // TODO:  Remove before production
+    devtool: 'source-map',
     entry: {
         global: './src/main/webapp/content/scss/global.scss',
         main: './src/main/webapp/app/app.main'
