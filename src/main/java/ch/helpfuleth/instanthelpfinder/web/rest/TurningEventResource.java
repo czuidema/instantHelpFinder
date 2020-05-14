ba461e11eb658a1a210eef215ac41812bb5fd660
Georg Gruetzner <g.gruetzner@protonmail.com>
2020-05-14 19:56:07 +0200

    Adds REST API methond to return all tasks for a specific candidate group

--- src/main/java/ch/helpfuleth/instanthelpfinder/web/rest/TurningEventResource.java
+++ src/main/java/ch/helpfuleth/instanthelpfinder/web/rest/TurningEventResource.java
@@ -9,19 +9,18 @@ import ch.helpfuleth.instanthelpfinder.web.rest.errors.BadRequestAlertException;
 import io.github.jhipster.web.util.HeaderUtil;
 import io.github.jhipster.web.util.ResponseUtil;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 /**
  * REST controller for managing {@link ch.helpfuleth.instanthelpfinder.domain.TurningEvent}.
@@ -144,6 +143,29 @@ public class TurningEventResource {
         log.debug("REST request to get TurningEvent : {}", id);
         Optional<TurningEvent> turningEvent = turningEventRepository.findOneWithEagerRelationships(id);
         return ResponseUtil.wrapOrNotFound(turningEvent);
+    }
+
+    // get tasks for a candidate group, candidateGroupName can be Doctors or Assistants
+    /**
+     * {@code GET  /tasks/:candidateGroupName} : get all the turningEvents associated to candidateGroupName.
+     *
+     * @param candidateGroupName name of the candidate group.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and the list of turningEvents in body.
+     */
+    @GetMapping("/tasks/{candidateGroupName}")
+    public List<TurningEvent> getCandidateGroupTasks(@PathVariable String candidateGroupName) {
+        log.debug("REST request to get TurningEvents for a specific candidate group : {}", candidateGroupName);
+
+        List<Task> tasks = flowableService.getCandidateGroupTasks(candidateGroupName);
+        List<TurningEvent> turningEvents = new ArrayList<TurningEvent>();
+        for (Task task : tasks) {
+            Map<String,Object> processVariables = task.getProcessVariables();
+            // get turningEventId from task
+            Long turningEventId = Long.valueOf(processVariables.get("turningEventId").toString());
+            TurningEvent turningEvent = turningEventRepository.getOne(turningEventId);
+            turningEvents.add(turningEvent);
+        }
+        return turningEvents;
     }
 
     /**
